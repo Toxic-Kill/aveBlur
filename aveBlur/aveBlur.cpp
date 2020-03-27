e072e1350c48bd8a59ca4648428cbc83156a15d5
Kill Toxic <913087364@qq.com>
2020-03-27 14:22:21 +0800

    aveBlur

--- aveBlur/aveBlur.cpp
+++ aveBlur/aveBlur.cpp
@@ -1,20 +1,20 @@
-﻿// aveBlur.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#include <iostream>
+#include<opencv2/opencv.hpp>
 
-#include <iostream>
+using namespace std;
+using namespace cv;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	VideoCapture cap(0);//调用摄像头
+	while (1)
+	{
+		cv::Mat frame;
+		cv::Mat detectMat;
+		cap >> frame;//捕捉当前帧
+		blur(frame, detectMat, Size(3, 3));//进行均值滤波处理
+		cv::imshow("frame", frame);//显示当前图像
+		cv::imshow("aveBlur", detectMat);//显示均值滤波处理后图像
+		waitKey(30);
+	}
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
